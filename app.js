f74302d8215f11cd25e370f38f140b814d0151ef
jannesurborg <janne.surborg@hotmail.de>
2026-05-19 20:33:06 +0200

    generate random x values -2,2 N= 100

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.log("TensorFlow.js geladen:", tf);
 console.log("Plotly geladen:", Plotly);
+
+//Generieren von N=100 zufälligen, gleich-verteilten x Werte aus dem Intervall [-2,+2]
+
+const DATASET_SIZE = 100;
+
+const xValues = [];
+
+for (let i = 0; i < DATASET_SIZE; i++) {
+
+    const x = Math.random() * 4 - 2;
+
+    xValues.push(x);
+}
+
+console.log(xValues);
