94f12425fc377efb3357a063e04e85d7d773b718
jannesurborg <janne.surborg@hotmail.de>
2026-05-19 21:01:29 +0200

    generate y values

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ console.log("TensorFlow.js geladen:", tf);
 console.log("Plotly geladen:", Plotly);
 
 //Generieren von N=100 zufälligen, gleich-verteilten x Werte aus dem Intervall [-2,+2]
+//Berechnen der dazugehörigen y-Werte
 
 const DATASET_SIZE = 100;
 
 const xValues = [];
+
+function calculateY(x) {
+
+    return (
+        0.5 *
+        (x + 0.8) *
+        (x + 1.8) *
+        (x - 0.2) *
+        (x - 0.3) *
+        (x - 1.9)
+        + 1
+    );
+}
 
 for (let i = 0; i < DATASET_SIZE; i++) {
 
@@ -14,4 +28,16 @@ for (let i = 0; i < DATASET_SIZE; i++) {
     xValues.push(x);
 }
 
-console.log(xValues);
+const dataset = [];
+
+for (const x of xValues) {
+
+    const y = calculateY(x);
+
+    dataset.push({
+        x: x,
+        y: y
+    });
+}
+
+console.log(dataset);
